7980774dcf8b843b350ad7800387b96bd6a20467
alieuroshub001 <alirayyan.euroshub@gmail.com>
2025-05-28 01:16:42 +0500

    mongo

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,20 +11,16 @@ if (!process.env.MONGODB_URI) {
 }
 
 declare global {
-  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so the connection
-  // is preserved across module reloads caused by HMR (Hot Module Replacement)
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
