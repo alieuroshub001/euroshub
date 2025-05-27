fd6698f4724355b0183e98f1c04b184e613e1e27
alieuroshub001 <alirayyan.euroshub@gmail.com>
2025-05-27 08:34:29 +0500

    cop

--- src/app/admin/jobs/edit/[id]/page.tsx
+++ src/app/admin/jobs/edit/[id]/page.tsx
@@ -1,11 +1,11 @@
 import AdminLayout from '@/components/adminpanel/AdminLayout';
 import JobForm from '@/components/adminpanel/JobForm';
 
-type PageProps = {
+interface PageProps {
   params: { id: string };
-};
+}
 
-export default function AdminEditJobPage({ params }: PageProps) {
+export default function AdminEditJobPage({ params }: { params: { id: string } }) {
   return (
     <AdminLayout>
       <JobForm jobId={params.id} />
