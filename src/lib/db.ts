a65899ed8ea8eab08ea2f486357de241b1f7555f
alieuroshub001 <alirayyan.euroshub@gmail.com>
2025-05-29 23:51:37 +0500

    ok

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,16 +13,20 @@ if (!process.env.MONGODB_URI) {
 }
 
 declare global {
+  // Using var to augment global object (required for TypeScript global cache)
+  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (process.env.NODE_ENV === 'development') {
+  // Use cached client in development to prevent multiple connections
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production, avoid using global cache
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
